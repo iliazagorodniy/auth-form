95d65572b4436a1bb1876733b8dd3a53f5ca9eea
Ilia <izagorodn@gmail.com>
2020-07-15 19:36:41 +0300

    adaded adaptive design and animations

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hellow");
+let headerColumn = document.querySelector(".header-column")
+let formColumn = document.querySelector(".form-column")
+
+if (window.innerWidth <= 1000) {
+    headerColumn.classList.replace("eight", "sixteen")
+    formColumn.classList.replace("eight", "sixteen")
+}
+
+window.addEventListener('resize', changeClass)
+
+function changeClass() {
+    if (window.innerWidth <= 1000) {
+        headerColumn.classList.replace("eight", "sixteen")
+        formColumn.classList.replace("eight", "sixteen")
+    } else {
+        headerColumn.classList.replace("sixteen", "eight")
+        formColumn.classList.replace("sixteen", "eight")
+    }
+}
